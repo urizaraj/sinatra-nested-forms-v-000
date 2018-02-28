0f7e1e67a9267ddacf97edb4241560bffbd77e43
Alexander Urizar <urizaraj@gmail.com>
2018-02-28 23:00:35 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(hash)
+    self.name = hash[:name]
+    self.weight = hash[:weight]
+    self.height = hash[:height]
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
+  def initialize(hash)
+    self.name = hash[:name]
+    self.type = hash[:type]
+    self.booty = hash[:booty]
+
+    @@all << self
+  end
 end
